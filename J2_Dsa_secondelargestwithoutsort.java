635f694fea1b34b268cb9113dc500ed558636e20
sunilj07022005 <sunilj07022005@gmail.com>
2026-09-19 15:17:06 +0530

    find seconde largest without sorting array

--- J2_Dsa_secondelargestwithoutsort.java
+++ J2_Dsa_secondelargestwithoutsort.java
@@ -2,49 +2,51 @@
 
 public class J2_Dsa_secondelargestwithoutsort 
 {
-    // public  static  void main(String arg [])
-    // {
-    //     int arr[] = {23,4,64,34,74,85,74,8};
-    //     int largest = arr[0];
-    //     int seclargest = arr[1];
-    //     // int size = arr.length;
-    //     for(int a=0; a<arr.length; a++)
-    //     {
-    //         if(arr[a] > largest)
-    //         {
-    //             seclargest = largest;
-    //             largest = arr[a];
-    //         }
-    //     }
-    //     System.out.println(largest);
-    //     System.out.println(seclargest);
-    // }
-
-    public static  void main(String arg [])
+    public  static  void main(String arg [])
     {
         int arr[] = {23,4,64,34,74,85,74,8};
-
-        int smallest = arr[0];
-        int secsmallest = arr[1];
-        if (secsmallest < smallest ) {
-            int temp = smallest;
-            smallest = secsmallest;
-            secsmallest = temp;
-        }
-        for(int a=2; a<arr.length; a++)
+        int largest = arr[0];
+        int seclargest = arr[1];
+        // int size = arr.length;
+        for(int a=0; a<arr.length; a++)
         {
-            if(arr[a]<smallest)
+            if(arr[a] > largest)
             {
-                secsmallest = smallest;
-                smallest = arr[a];
+                seclargest = largest;
+                largest = arr[a];
             }
-            else if (arr[a] < secsmallest)   // if two small same value are present and you want to allow then uswe the <= operater 
-            {
-                secsmallest = arr[a];
+            else if(arr[a] >seclargest ){
+                seclargest = arr[a];
             }
         }
-        System.out.println("smallest : "+smallest);
-        System.out.println("secsmallest : "+secsmallest);
+        System.out.println(largest);
+        System.out.println(seclargest);
     }
-}
 
+    // public static  void main(String arg [])
+    // {
+    //     int arr[] = {23,4,64,34,74,85,74,8};
+
+    //     int smallest = arr[0];
+    //     int secsmallest = arr[1];
+    //     if (secsmallest < smallest ) {
+    //         int temp = smallest;
+    //         smallest = secsmallest;
+    //         secsmallest = temp;
+    //     }
+    //     for(int a=2; a<arr.length; a++)
+    //     {
+    //         if(arr[a]<smallest)
+    //         {
+    //             secsmallest = smallest;
+    //             smallest = arr[a];
+    //         }
+    //         else if (arr[a] < secsmallest)   // if two small same value are present and you want to allow then uswe the <= operater 
+    //         {
+    //             secsmallest = arr[a];
+    //         }
+    //     }
+    //     System.out.println("smallest : "+smallest);
+    //     System.out.println("secsmallest : "+secsmallest);
+    // }
+}
